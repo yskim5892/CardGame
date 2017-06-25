d8906e6bf00492c2cd41448639b9d2a7d02b443e
Kim Yoonsung <yskim227@snu.ac.kr>
2017-06-26 01:32:20 +0900

    first

---
+++ src/Pile.py
@@ -0,0 +1,41 @@
+import random
+
+class Pile:
+    def __init__(self):
+        pass
+
+    def shuffle(self):
+        try:
+            random.shuffle(self.cards)
+        except AttributeError:
+            return
+
+    def addCard(self, card):
+        try:
+            self.cards.append(card)
+        except AttributeError:
+            self.cards = [card]
+
+
+    def drawCard(self):
+        try:
+            return self.cards.pop(-1)
+        except AttributeError:
+            return
+
+a = Pile()
+
+a.addCard(3)
+a.addCard(7)
+a.addCard(23)
+a.addCard(25)
+
+print(a.cards)
+
+a.shuffle()
+
+print(a.cards)
+
+print(a.drawCard())
+print(a.drawCard())
+print(a.cards)
